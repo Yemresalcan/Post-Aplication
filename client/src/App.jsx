f8442733c3c13097fc4a55ab1402450192836e66
Yunus emre salcan <yunusemresalcan@gmail.com>
2023-11-08 15:38:29 +0300

    Categoires Products CartTotals components and some responsive update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
+import Categories from "./components/categories/Categories.jsx";
+import Products from "./components/products/products.jsx";
 import Header from "./components/header/index.jsx";
+import CartTotals from "./components/cart/CartTotals.jsx";
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      home
-    </div>
+      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
+        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
+          <Categories />
+        </div>
+        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
+          <Products />
+        </div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals />
+        </div>
+
+     </div>
+    </>
   );
 }
 
