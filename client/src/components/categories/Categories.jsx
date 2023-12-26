904564f0d044a212e94a3932a1fc111c08751bd3
Yunus emre salcan <yunusemresalcan@gmail.com>
2023-12-26 23:46:55 +0300

    Category Client API

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,34 +1,56 @@
-import React from 'react'
+import { PlusOutlined,EditOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { Button, Form, Input, message, Modal } from "antd";
+import "./style.css";
+import Add from "./Add";
+import Edit from "./Edit";
 
-const Categories = () => {
+const Categories = ({categories,setCategories}) => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [form] = Form.useForm();
+
+
   return (
     <ul className="flex gap-4 md:flex-col text-lg">
-    <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-      <span>Tümü</span>
-    </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>Yiyecek</span>
+    {categories.map((item) => (
+      <li className="category-item" key={item._id}>
+        <span>{item.title}</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
+    ))}
+
+
+
+
+      <li
+        className="category-item !bg-orange-500 hover:opacity-90"
+        onClick={() => setIsAddModalOpen(true)}
+      >
+        <PlusOutlined className="md:text-2xl" />
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
+      <li
+        className="category-item !bg-red-500 hover:opacity-90"
+        onClick={() => setIsEditModalOpen(true)}
+      >
+        <EditOutlined className="md:text-2xl" />
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
-      </li>
+      <Add 
+        isAddModalOpen={isAddModalOpen}
+        setIsAddModalOpen={setIsAddModalOpen}
+        categories={categories}
+        setCategories={setCategories}
+        />
+
+<Edit
+        isEditModalOpen={isEditModalOpen}
+        setIsEditModalOpen={setIsEditModalOpen}
+        categories={categories}
+        setCategories={setCategories}
+      />
+
+
     </ul>
-  )
-}
+  );
+};
 
 export default Categories
